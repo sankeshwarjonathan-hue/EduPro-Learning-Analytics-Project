4229bf9dbd87c724b1b01ffdaf9c578a1acd3d05
sankeshwarjonathan-hue <sankeshwarjonathan@gmail.com>
2026-05-08 12:04:53 +0530

    matplot error fiexed

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 st.set_page_config(page_title="EduPro Personalization", layout="wide")
 

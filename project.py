a2c0e8774673e44778873b86f94b8782fb1514ef
sankeshwarjonathan-hue <sankeshwarjonathan@gmail.com>
2026-05-08 12:11:42 +0530

    add req file

--- project.py
+++ project.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 
 st.set_page_config(page_title="EduPro Personalization", layout="wide")
 
